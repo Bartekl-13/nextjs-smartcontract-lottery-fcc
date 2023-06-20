2ab4b69d13271c394ad23b156d06f9cf3aa32185
root <root@BARTEK-PC>
2023-06-20 09:25:33 +0200

    some static changes to connect button

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,10 @@ export default function Header() {
         <div className="p-5 border-b-2 flex flex-row">
             <h1 className="py-4 px-4 font-blog text-3xl">Decentralized Lottery</h1>
             <div className="ml-auto py-2 px-4">
-                <ConnectButton moralisAuth={false} />
+                <ConnectButton
+                    className="rounded-full bg-slate-300 text-sky-600"
+                    moralisAuth={false}
+                />
             </div>
         </div>
     )
